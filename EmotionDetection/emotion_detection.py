37eaff07f0b93c2c3a9deee12e71839452e4219f
FauziCoursera <ahmadfauzirohman@outlook.com>
2026-06-06 22:30:32 -0400

    application make

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -8,8 +8,17 @@ def emotion_detector(text_to_analyze):
     
     response = requests.post(url, json=myobj, headers=headers)
     
+    if response.status_code == 400:
+        return {
+            'anger': None,
+            'disgust': None,
+            'fear': None,
+            'joy': None,
+            'sadness': None,
+            'dominant_emotion': None
+        }
+
     formatted_response = json.loads(response.text)
-    
     emotion_predictions = formatted_response['emotionPredictions'][0]['emotion']
     
     anger_score = emotion_predictions['anger']
